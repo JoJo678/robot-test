c0cb5bfb551589c7d5bc23dd64d8bb5bae135dd9
Powerz <jordan.divito.pro@gmail.com>
2019-03-07 11:50:10 +0100

    Fix see all robot in home

--- src/main/java/com/example/robotrestserverapi/model/Robot.java
+++ src/main/java/com/example/robotrestserverapi/model/Robot.java
@@ -29,6 +29,14 @@ public class Robot {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
 	public String getName() {
 		return name;
 	}
